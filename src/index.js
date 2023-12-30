dec3b24b391a7a570e31c91b42df9ff1b1820b14
Rohitdey2697 <rohit.dey2697@gmail.com>
2023-12-30 23:32:54 +0530

    added cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express()
+const cors = require('cors')
 const bodyParser = require('body-parser')
 require('./database/connection')
 const UserRouter = require('./router/user')
 const TaskRouter = require('./router/task')
 const port = process.env.PORT;
 app.use(bodyParser.json())
+app.use(cors({
+    origin: "*"
+}))
 app.use(TaskRouter)
 app.use(UserRouter)
 
